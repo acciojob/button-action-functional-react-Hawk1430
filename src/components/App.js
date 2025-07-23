af07baf15354196c3b9904be1859643844ab2df4
Prashant Raj <pr2125116@gmail.com>
2025-07-23 15:34:36 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,23 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [toggle, setToggle] = useState(false);
+	const [toggle, setToggle] = useState(false);
 
-  return (
-    <div id="main">
-      // Do not alter the main div
-      <button id="click"  onClick={() => setToggle(!toggle)}>Click Me</button>
-      <p id="para" style={{display: toggle ? 'block' : 'none'}}>
-        Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
-      </p>
-    </div>
-  );
+	return (
+		<div id="main">
+			// Do not alter the main div
+			<button id="click" onClick={() => setToggle(!toggle)}>
+				Click Me
+			</button>
+			{toggle && (
+				<p id="para">
+					Hello, I've learnt to use the full-stack evaluation tool.
+					This makes me so happy
+				</p>
+			)}
+		</div>
+	);
 }
-
 
 export default App;
